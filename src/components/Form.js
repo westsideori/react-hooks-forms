368ce92e6125266d414a8aa2125c71b49c508fe3
Ori Markowitz <ori.markowitz@gmail.com>
2021-02-02 19:19:24 -0500

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,28 @@
 import React, { useState } from "react";
+import DisplayData from "./DisplayData"
 
 function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+  
+  function handleLastNameChange(event) {
+    console.log(event.target.value)
+    setLastName(event.target.value);
+  } 
+
   return (
-    <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} /> 
+        <button type="submit">Submit</button>
+      </form>
+      <DisplayData firstName={firstName} lastName={lastName} />
+    </div>
   );
 }
 
